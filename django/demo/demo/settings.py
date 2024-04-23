55558681d21989bc2edef130019565718048ed1e
Илья Белезяков <belezyakov_in@macbook-air-ilia.local>
2024-04-23 20:03:15 +0300

    Cut off nginx service and gunicorn

--- django/demo/demo/settings.py
+++ django/demo/demo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2&9=a#bu*%a$6&!kxzemp9lb%w)^hm)0&ko(y^$q427t2#lcik
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [192.168.3.92]
+ALLOWED_HOSTS = ['192.168.3.92']
 
 
 # Application definition
